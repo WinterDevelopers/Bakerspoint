cdfbb576670710f7c9595a88b38da1e0b7cf98b1
Christian <winterdevelopers@gmail.com>
2022-08-22 14:37:39 +0100

    finish the index page

--- company/static/js/sctipt.js
+++ company/static/js/sctipt.js
@@ -1,11 +1,20 @@
 console.log('Bakerspoint')
 
-let footer_img = document.querySelectorAll(".footer-img")
+let footer_img = document.querySelectorAll('[name="footer-img"]')
+let footer_p_item = document.querySelectorAll('[name="footer-dropdown"]')
+console.log(footer_p_item)
 
 for(let a = 0; a < footer_img.length; a++){
 
     footer_img[a].addEventListener("click", function(){
-        footer_img[a].className ='rotate-90'
+        if(footer_img[a].className !='rotate-90'){
+            footer_img[a].className = 'rotate-90'
+            footer_p_item[a].className = 'footer-content-p-animate footer-content-p'
+        }else{
+            footer_img[a].className = 'reverse-rotate-90'
+            footer_p_item[a].className = 'no-display-min-animate'
+            
+        }
     })
 }
 
